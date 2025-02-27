337aaecd9706a45546d6c7f240bf4139063e630f
MULIPUJITHA <136082749+MULIPUJITHA@users.noreply.github.com>
2025-02-27 19:46:55 +0530

    Create indexdeclerative.js

---
+++ indexdeclerative.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Select root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App component inside root
+root.render(<App />);
